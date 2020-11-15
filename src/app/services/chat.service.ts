5837b93a4e784ab0981aee97a6bd50cb25493cdb
Estuardo <estuardo1314@gmail.com>
2020-11-15 00:34:24 -0600

    Ngrok agregado para crear tunnel hacía localhost

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -29,7 +29,7 @@ export class ChatService {
 
   public createConnection(){
     this.connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://localhost:44363/socket")
+    .withUrl("http://c7038007df6d.ngrok.io/socket")
     .configureLogging(signalR.LogLevel.Information)
     .build();
   }
